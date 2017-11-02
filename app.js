c7a14e1cf27c773edb725106d08fdf9a4b45a746
David Janas <davidjanasr@gmail.com>
2017-11-02 15:14:09 -0400

    Allow pug to use CSS from static directory

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const morgan = require('morgan');
 
 
 app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, 'styles')));
 app.set('views', './views');
 app.set('view engine', 'pug');
-app.get('/', (req, res) => res.render('hello', { name: "David"}));
+app.get('/', (req, res) => res.render('index', { name: "David"}));
 
 app.listen(3000, () => console.log("listening on port 3000"));
