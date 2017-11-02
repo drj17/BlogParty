74a6703194a839cd42d90fb266eac866a7d0f51b
David Janas <davidjanasr@gmail.com>
2017-11-02 16:12:06 -0400

    create post route and form for creating new blog post

--- app.js
+++ app.js
@@ -1,13 +1,37 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
+const blogFile = fs.readFileSync('./seeds/blogs.json', 'utf-8');
+const blogArray = JSON.parse(blogFile);
 
 
 app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static(path.join(__dirname, 'styles')));
 app.set('views', './views');
 app.set('view engine', 'pug');
-app.get('/', (req, res) => res.render('index', { name: "David"}));
+
+app.get('/', (req, res) => {
+  res.render('index', {
+    blogs: blogArray
+  });
+});
+
+app.get('/new', (req, res) => {
+  res.render('new');
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+  blogArray.push(req.body);
+
+  fs.writeFile('./seeds/blogs.json', JSON.stringify(blogArray));
+  res.redirect('/');
+});
 
 app.listen(3000, () => console.log("listening on port 3000"));
